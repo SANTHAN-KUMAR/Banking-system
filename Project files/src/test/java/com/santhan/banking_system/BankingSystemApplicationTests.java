17e39b06bfc738e56d562a4eec203e7d93a366ba
SANTHAN-KUMAR <74782855+SANTHAN-KUMAR@users.noreply.github.com>
2025-06-05 13:11:39 +0530

    Update BankingSystemApplicationTests.java

--- Project files/src/test/java/com/santhan/banking_system/BankingSystemApplicationTests.java
+++ Project files/src/test/java/com/santhan/banking_system/BankingSystemApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser; // NEW import
 
 @SpringBootTest
+@ActiveProfiles("test") // Add this line
 class BankingSystemApplicationTests {
 
 	@Test
