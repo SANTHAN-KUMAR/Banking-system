51aee48bbc20eda21bf46b6bc1279f5ef2835013
SANTHAN-KUMAR <74782855+SANTHAN-KUMAR@users.noreply.github.com>
2025-06-05 13:16:38 +0530

    Update BankingSystemApplicationTests.java

--- Project files/src/test/java/com/santhan/banking_system/BankingSystemApplicationTests.java
+++ Project files/src/test/java/com/santhan/banking_system/BankingSystemApplicationTests.java
@@ -3,14 +3,14 @@ package com.santhan.banking_system;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser; // NEW import
+import org.springframework.test.context.ActiveProfiles; // Add this import
 
 @SpringBootTest
 @ActiveProfiles("test") // Add this line
 class BankingSystemApplicationTests {
 
-	@Test
-	@WithMockUser
-	void contextLoads() {
-	}
-
+    @Test
+    @WithMockUser
+    void contextLoads() {
+    }
 }
